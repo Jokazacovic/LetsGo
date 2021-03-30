2ffcb3e50aa4fd23dd7ee1aa73546e183b6f2e18
Sumin Kim <ppsm920@gmail.com>
2021-03-30 15:57:23 -0700

    in Process in Input File

--- client/components/inputPage.jsx
+++ client/components/inputPage.jsx
@@ -1,131 +1,60 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { Component } from 'react';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css'
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Location : '',
+      Date: '',
 
-export default function Input(props) {
-  
-  const handleChange = () => {
+    };
+  }
 
-  };
-  
-  const handleClick = () => {
-  };
+  render() {
+    const lists = ['Breakfast', 'Lunch', 'Dinner', 'Hotels', 'Active Life', 'Arts & Entertainment', 'Nightlife', 'Shopping'];
+    const checkLists = lists.map((element) => (
+      <div>
+        <label>
+          <input name={element} type="checkbox" />
+          {element}
+        </label>
+      </div>
+    ));
 
-  return (
-    <div className="addDay">
-      <form action="" className="dayForm">
-        <div className="location">
-          <label className="cardFormLabel">Location</label>
-          <input type="text" className="cardFormInput" />
-        </div>
-        <div className="date">
-          <label className="cardFormLabel">Date</label>
-          <input type="text" className="cardFormInput" />
-        </div>
-        <div className="radius">
-          <select>
-            <option selected value="Walking"> Walking(1mi)</option>
-            <option value="Biking"> Biking(2mi) </option>
-            <option value="Driving5">Driving(5mi)</option>
-            <option value="Driving10">Driving(10mi)</option>
-          </select>
-        </div>
-        <div className="budget">
-          {/* <label className="cardFormLabel">Budget</label>
-          <input type="text" className="cardFormInput" /> */}
-          <select>
-            <option selected value="Walking">$</option>
-            <option value="Biking"> $$ </option>
-            <option value="Driving5">$$$</option>
-            <option value="Driving10">$$$$</option>
-          </select>
-        </div>
-        <label>
-          Food
-          <br />
-          <input
-            name="breakfast"
-            type="checkbox"
-          />
-          Breakfast & Brunch
-        </label>
-        <br />
-        <label>
-          <input
-            name="lunch"
-            type="checkbox"
-          />
-          Lunch
-        </label>
-        <br />
-        <label>
-          <input
-            name="dinner"
-            type="checkbox"
-          />
-          Dinner
-        </label>
-        <br />
-        <label>
-          Activity
-          <br />
-          <input
-            name="hotels"
-            type="checkbox"
-          />
-          Hotels
-        </label>
-        <br />
-        <label>
-          <input
-            name="activeLife"
-            type="checkbox"
-          />
-          Active Life
-        </label>
-        <br />
-        <label>
-          <input
-            name="entertainment"
-            type="checkbox"
-          />
-          Arts & Entertainment
-        </label>
-        {/* <br /> */}
-        {/* <label>
-          <input
-            name="beauty"
-            type="checkbox"
-          />
-          Beauty & Spas
-        </label> */}
-        {/* <br />
-        <label>
-          <input
-            name="localFlavor"
-            type="checkbox"
-          />
-          Local Flavor
-        </label> */}
-        <br />
-        <label>
-          <input
-            name="nightlife"
-            type="checkbox"
-          />
-          Nightlife
-        </label>
-        <br />
-        <label>
-          <input
-            name="shopping"
-            type="checkbox"
-          />
-          Shopping
-        </label>
-        <br />
-      </form>
-      <button className="addDayBtn" type="button">Create a New Trip</button>
-    </div>
-  );
+    return (
+      <div className="addDay">
+        <form action="" className="dayForm">
+          <div className="Location">
+            <label className="cardFormLabel">Location</label>
+            <input type="text" className="cardFormInput" />
+          </div>
+          <div className="Data">
+          <DatePicker selected={startdate} onChange={date => setStartDate(date)} />
+          </div>
+          <div className="radius">
+            <select>
+              <option selected value="Walking"> Walking(1mi)</option>
+              <option value="Biking"> Biking(2mi) </option>
+              <option value="Driving5">Driving(5mi)</option>
+              <option value="Driving10">Driving(10mi)</option>
+            </select>
+          </div>
+          <div className="budget">
+            <select>
+              <option selected value="$">$</option>
+              <option value="$$"> $$ </option>
+              <option value="$$$">$$$</option>
+              <option value="$$$$">$$$$</option>
+            </select>
+          </div>
+          {checkLists}
+        </form>
+        <button className="addDayBtn" type="button">Create a New Trip</button>
+      </div>
+    );
+  }
 }
 
 // categories:
@@ -136,3 +65,4 @@ export default function Input(props) {
 // Local Flavor (localflavor, All)
 // Nightlife (nightlife, All)
 // Shopping (shopping, All)
+export default Input;
