1b28681846805441b81d398237912099641a3306
Jonathan <jonathan@Kias-MacBook-Pro.local>
2021-04-01 17:33:15 -0700

    Server able to make requests
    Co-authored-by: Zach Brucker <zach.brucker@gmail.com>
    Co-authored-by: Sumin Kim <ppsm920@gmail.com>
    Co-authored-by: Vicki Lee <leevicki01@gmail.com>
    Co-authored-by: Cole Redfearn <coleredfearn@gmail.com>

--- client/components/inputPage.jsx
+++ client/components/inputPage.jsx
@@ -5,14 +5,6 @@ import TextField from '@material-ui/core/TextField';
 import { TripContext } from '../contexts/TripContext';
 
 export default function InputFor() {
-  // const [ location, setLocation ] = useState('');
-  // const [ date, setDate ] = useState('2021-04-01')
-  // const [radius, setRadius] = useState(1600);
-  // const [budget, setBudget] = useState('$');
-  // const [Breakfast, setBreakfast] = useState('');
-  // const [Lunch, setLunch] = useState(false);
-  // const [] = useState(false);
-  // const [] = useState('');
   let history = useHistory();
   const [tripInfo, setTripInfo] = useContext(TripContext);
   const [input, setInput] = useState({
@@ -29,28 +21,6 @@ export default function InputFor() {
     Nightlife: false,
     Shopping: false,
   });
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     location: '',
-  //     date: '2021-04-01',
-  //     radius: 1600,
-  //     budget: '$',
-  //     Breakfast: false,
-  //     Lunch: false,
-  //     Dinner: false,
-  //     Hotels: false,
-  //     'Active Life': false,
-  //     'Arts & Entertainment': false,
-  //     Nightlife: false,
-  //     Shopping: false,
-  //   };
-
-  // this.changeHandler = this.changeHandler.bind(this);
-  // this.forCheckList = this.forCheckList.bind(this);
-  // this.handleClick = this.handleClick.bind(this);
-  // }
 
   // onClick functionality for submit button
   function handleClick(e) {
@@ -87,16 +57,10 @@ export default function InputFor() {
 
   // onChange handlers for Location and Date with inputted value
   function changeHandler(e) {
-    console.log('e.target.name: ', e.target.name);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
-    console.log('input after setInput: ', input);
-    // e.target.name = date || location
-    // setState((state) => ({
-    //   [e.target.name]: e.target.value,
-    // }));
   }
 
   // onChange handlers for checkboxes
@@ -114,8 +78,6 @@ export default function InputFor() {
       });
     }
   }
-
-  // render() {
   // Map Pre-Determined Yelp Categories to React Checkboxes
   const lists = [
     'Breakfast',
@@ -197,164 +159,3 @@ export default function InputFor() {
     </div>
   );
 }
-// }
-
-// class Input extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       location: '',
-//       date: '2021-04-01',
-//       radius: 1600,
-//       budget: '$',
-//       Breakfast: false,
-//       Lunch: false,
-//       Dinner: false,
-//       Hotels: false,
-//       'Active Life': false,
-//       'Arts & Entertainment': false,
-//       Nightlife: false,
-//       Shopping: false,
-//     };
-
-//     this.changeHandler = this.changeHandler.bind(this);
-//     this.forCheckList = this.forCheckList.bind(this);
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   // onClick functionality for submit button
-//   handleClick(e) {
-//     const vals = Object.values(this.state);
-//     let sendForm = false;
-//     // check state for checkboxes with 'true' values
-//     vals.forEach((ele) => {
-//       if (ele.toString() === 'true') sendForm = true;
-//     });
-//     // if at least one activity is checked, fetch POST request
-//     if (sendForm) {
-//       fetch('/itinerary', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(this.state),
-//       })
-//         .then((res) => res.json())
-//         .then(() => {
-//           const { history } = this.props;
-//           history.push('/landing');
-//         })
-//         .catch((err) =>
-//           console.log('error has occurred in Fetching this.state')
-//         );
-//     } else {
-//       alert('Check at least one activity!');
-//     }
-//   }
-
-//   // onChange handlers for Location and Date with inputted value
-//   changeHandler(e) {
-//     this.setState((state) => ({
-//       [e.target.name]: e.target.value,
-//     }));
-//   }
-
-//   // onChange handlers for checkboxes
-//   forCheckList(e) {
-//     const property = e.target.name;
-//     if (this.state[property]) {
-//       this.setState({
-//         [property]: false,
-//       });
-//     } else {
-//       this.setState({
-//         [property]: true,
-//       });
-//     }
-//   }
-
-//   render() {
-//     // Map Pre-Determined Yelp Categories to React Checkboxes
-//     const lists = [
-//       'Breakfast',
-//       'Lunch',
-//       'Dinner',
-//       'Hotels',
-//       'Active Life',
-//       'Arts & Entertainment',
-//       'Nightlife',
-//       'Shopping',
-//     ];
-//     const checkLists = lists.map((element) => (
-//       <div>
-//         <label>
-//           <input
-//             name={element}
-//             type='checkbox'
-//             defaultChecked={this.state[element]}
-//             onChange={this.forCheckList}
-//           />
-//           {element}
-//         </label>
-//       </div>
-//     ));
-
-//     return (
-//       <div className='addDay'>
-//         <form action='' className='dayForm'>
-//           <div className='Location'>
-//             <label className='cardFormLabel'>Location</label>
-//             <input
-//               type='text'
-//               className='cardFormInput'
-//               name='location'
-//               onChange={this.changeHandler}
-//               required
-//             />
-//           </div>
-//           <div className='Date'>
-//             <form className='date' noValidate>
-//               <TextField
-//                 id='date'
-//                 type='date'
-//                 defaultValue='2021-04-01'
-//                 InputLabelProps={{
-//                   shrink: true,
-//                 }}
-//                 name='date'
-//                 onChange={this.changeHandler}
-//               />
-//             </form>
-//           </div>
-//           <div className='radius'>
-//             <select name='radius' onChange={this.changeHandler}>
-//               <option selected value={1600}>
-//                 {' '}
-//                 Walking (1mi)
-//               </option>
-//               <option value={3200}> Biking (2mi) </option>
-//               <option value={8000}>Driving (5mi)</option>
-//               <option value={16000}>Driving (10mi)</option>
-//             </select>
-//           </div>
-//           <div className='budget'>
-//             <select name='budget' onChange={this.changeHandler}>
-//               <option selected value='$'>
-//                 $
-//               </option>
-//               <option value='$$'>$$</option>
-//               <option value='$$$'>$$$</option>
-//               <option value='$$$$'>$$$$</option>
-//             </select>
-//           </div>
-//           {checkLists}
-//         </form>
-//         <button className='addDayBtn' type='button' onClick={this.handleClick}>
-//           Create a New Trip
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Input;
--- client/components/landingPage.jsx
+++ client/components/landingPage.jsx
@@ -2,8 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TripContext } from '../contexts/TripContext';
 import Trip from './tripPage';
-import LoadingPage from "./LoadingPage";
-
+import LoadingPage from './LoadingPage';
 
 export default function Landing(props) {
   // Pull state into component from TripContext using 'useContext' hook
@@ -29,96 +28,8 @@ export default function Landing(props) {
       .catch((err) => console.log('error has occurred in Fetching TripInfo'));
   }, []);
 
-  // const newState = [
-  //   {
-  //     _id: 1,
-  //     date: '2020-05-30',
-  //     radius: 200,
-  //     location: 'Portland',
-  //     budget: '$$',
-  //     Breakfast: {
-  //       rating: 4,
-  //       price: '$',
-  //       phone: '+14152520800',
-  //       id: 'E8RJkjfdcwgtyoPMjQ_Olg',
-  //       alias: 'four-barrel-coffee-san-francisco',
-  //       is_closed: false,
-  //       categories: [
-  //         {
-  //           alias: 'coffee',
-  //           title: 'Coffee & Tea',
-  //         },
-  //       ],
-  //       review_count: 1738,
-  //       name: 'Four Barrel Coffee',
-  //       url: 'https://www.yelp.com/biz/four-barrel-coffee-san-francisco',
-  //       coordinates: {
-  //         latitude: 37.7670169511878,
-  //         longitude: -122.42184275,
-  //       },
-  //       image_url: 'http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg',
-  //       location: {
-  //         city: 'San Francisco',
-  //         country: 'US',
-  //         address2: '',
-  //         address3: '',
-  //         state: 'CA',
-  //         address1: '375 Valencia St',
-  //         zip_code: '94103',
-  //       },
-  //       distance: 1604.23,
-  //       transactions: ['pickup', 'delivery'],
-  //     },
-  //     Lunch: {
-  //       rating: 4,
-  //       price: '$',
-  //       phone: '+14152520800',
-  //       id: 'E8RJkjfdcwgtyoPMjQ_Olg',
-  //       alias: 'four-barrel-coffee-san-francisco',
-  //       is_closed: false,
-  //       categories: [
-  //         {
-  //           alias: 'coffee',
-  //           title: 'Coffee & Tea',
-  //         },
-  //       ],
-  //       review_count: 1738,
-  //       name: 'Four Barrel Coffee',
-  //       url: 'https://www.yelp.com/biz/four-barrel-coffee-san-francisco',
-  //       coordinates: {
-  //         latitude: 37.7670169511878,
-  //         longitude: -122.42184275,
-  //       },
-  //       image_url: 'http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg',
-  //       location: {
-  //         city: 'San Francisco',
-  //         country: 'US',
-  //         address2: '',
-  //         address3: '',
-  //         state: 'CA',
-  //         address1: '375 Valencia St',
-  //         zip_code: '94103',
-  //       },
-  //       distance: 1604.23,
-  //       transactions: ['pickup', 'delivery'],
-  //     },
-  //     Dinner: false,
-  //     Hotels: false,
-  //     Active: false,
-  //     Arts: false,
-  //     Nightlife: false,
-  //     Shopping: false,
-  //   }
-  // ];
-
-  // useEffect(() => {
-  //       // setLoading(false);
-  //       setTripInfo(newState);
-  //     }, []);
-
-
-   // if loading, ask for a state change
-   if (loading) {
+  // if loading, ask for a state change
+  if (loading) {
     return (
       <div>
         <LoadingPage />
@@ -127,7 +38,7 @@ export default function Landing(props) {
   }
 
   const eachTrip = tripInfo.slice(1).map((element, index) => (
-    <div className="eachTrip">
+    <div className='eachTrip'>
       <p>
         Location:
         {element.location}
@@ -137,18 +48,16 @@ export default function Landing(props) {
         {element.date}
       </p>
       <Link to={`/trip/${index}`} children={<Trip key={index} />}>
-        <button type="button">View Trip Details</button>
+        <button type='button'>View Trip Details</button>
       </Link>
     </div>
   ));
   return (
     <div>
-      <Link to="/input">
-        <button type="button">Create a new itinerary</button>
+      <Link to='/input'>
+        <button type='button'>Create a new itinerary</button>
       </Link>
-      <div className="trip-container">
-        {eachTrip}
-      </div>
+      <div className='trip-container'>{eachTrip}</div>
     </div>
   );
 }
